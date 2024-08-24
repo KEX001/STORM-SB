3f6377aeb7357b2eace9a265ac0631809cf1e8df
ᴋᴜɴᴀʟ <154862335+KEX001@users.noreply.github.com>
2024-08-24 13:49:36 +0530

    ᴠ3.1.1 [# ᴍᴀɪɴꜱ [ᴜᴘᴅᴀᴛᴇᴅ ᴛᴏ ᴘʀɪᴍᴇ]]

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ async def load_plugins_async(plugin_name):
         spec = importlib.util.spec_from_file_location(f"STORM.modules.{plugin_name}", path)
         load = importlib.util.module_from_spec(spec)
         load.logger = logging.getLogger(plugin_name)
-        await asyncio.to_thread(spec.loader.exec_module, load)l
+        await asyncio.to_thread(spec.loader.exec_module, load)
         sys.modules[f"STORM.modules.{plugin_name}"] = load
         print(f"[ꜱᴛᴏʀᴍ ᴀɪ] ʜᴀꜱ ɪᴍᴘᴏʀᴛᴇᴅ {plugin_name}")
     except Exception as e:
