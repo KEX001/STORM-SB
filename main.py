0f57236b7b29d646281670a54ccb4e6907d4d1c2
ᴋᴜɴᴀʟ <168260488+kxunal@users.noreply.github.com>
2024-08-24 13:56:11 +0530

    ᴠ3.1.1 [# ᴍᴀɪɴꜱ [ᴜᴘᴅᴀᴛᴇᴅ ᴛᴏ ᴘʀɪᴍᴇ]]

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ import glob
 import asyncio
 import logging
 import importlib.util
-import traceback
 import urllib3
 from pathlib import Path
 from config import KEX1, KEX2, KEX3, KEX4, KEX5, KEX6, KEX7, KEX8, KEX9, KEX10
@@ -11,30 +10,27 @@ from config import KEX1, KEX2, KEX3, KEX4, KEX5, KEX6, KEX7, KEX8, KEX9, KEX10
 logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s', level=logging.WARNING)
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
-async def load_plugins_async(plugin_name):
+def load_plugins(plugin_name):
     path = Path(f"STORM/modules/{plugin_name}.py")
     try:
         spec = importlib.util.spec_from_file_location(f"STORM.modules.{plugin_name}", path)
         load = importlib.util.module_from_spec(spec)
         load.logger = logging.getLogger(plugin_name)
-        await asyncio.to_thread(spec.loader.exec_module, load)
+        spec.loader.exec_module(load)
         sys.modules[f"STORM.modules.{plugin_name}"] = load
-        print(f"[ꜱᴛᴏʀᴍ ᴀɪ] ʜᴀꜱ ɪᴍᴘᴏʀᴛᴇᴅ {plugin_name}")
+        print(f"ꜱᴛᴏʀᴍ ʜᴀꜱ ɪᴍᴘᴏʀᴛᴇᴅ {plugin_name}")
     except Exception as e:
-        logging.error(f"[ꜱᴛᴏʀᴍ ᴀɪ] ꜰᴀɪʟᴇᴅ ᴛᴏ ʟᴏᴀᴅ ᴘʟᴜɢɪɴ {plugin_name}: {e}")
-        logging.error(traceback.format_exc())
+        print(f"ꜰᴀɪʟᴇᴅ ᴛᴏ ʟᴏᴀᴅ ᴘʟᴜɢɪɴ {plugin_name}: {e}")
 
-async def load_all_plugins():
-    files = [f for f in glob.glob("STORM/modules/*.py") if not f.endswith('__init__.py')]
-    tasks = []
-    for name in files:
-        patt = Path(name)
-        plugin_name = patt.stem
-        tasks.append(load_plugins_async(plugin_name))
-    await asyncio.gather(*tasks) 
+files = glob.glob("STORM/modules/*.py")
+for name in files:
+    patt = Path(name)
+    plugin_name = patt.stem
+    load_plugins(plugin_name)
+
+print("\nꜱᴛᴏʀᴍ ʙᴏᴛ ɪꜱ ᴅᴇᴘʟᴏʏᴇᴅ ꜱᴜᴄᴄᴇꜱꜰᴜʟʟʏ")
 
 async def main():
-    await load_all_plugins()  
     tasks = [
         KEX1.run_until_disconnected(),
         KEX2.run_until_disconnected(),
@@ -50,4 +46,10 @@ async def main():
     try:
         await asyncio.gather(*tasks)
     except Exception as e:
-        logging.error(f"[ꜱᴛᴏʀᴍ ᴀɪ] ꜰᴏᴜɴᴅ ᴀɴ ᴇʀʀᴏʀ ⚠️: {e}")
+        logging.error(f"ꜱᴛᴏʀᴍ ᴀɪ ꜰᴏᴜɴᴅ ᴀɴ ᴇʀʀᴏʀ ⚠️: {e}")
+
+loop = asyncio.get_event_loop()
+try:
+    loop.run_until_complete(main())
+finally:
+    loop.close()
